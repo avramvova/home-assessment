e78418db8e8594de9d1d889a77ac215a88ed04e4
avramvova <111857294+avramvova@users.noreply.github.com>
2022-08-23 11:51:18 +0300

    Add files via upload

---
+++ task1.cpp
@@ -0,0 +1,49 @@
+/******************************************************************************
+
+                              Online C++ Compiler.
+               Code, Compile, Run and Debug C++ program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <iostream>
+
+using namespace std;
+int data = 255;
+int payload = 6.153;
+bool flag = true;
+char ch = '#';
+int main()
+{
+  cout << "data value is : 255" << endl;
+  cout << "payload value is : 6.153" << endl;
+  cout << "flag value is : true" << endl;
+  cout << "ch value is : #" << endl;
+double a = 25;
+a = a+10;
+cout << a << endl;
+a = a + 100;
+cout << a << endl;
+a = a-20;
+cout << a << endl;
+a = a*3;
+cout << a << endl;
+a = a/2;
+cout << a << endl;
+   
+   float g = 0;
+   float h = 1; // ; 
+   double q = 1,5426; // , = . 
+    
+   
+    int value = 3;
+    value = value + 1;
+    cout << Value << endl; // cout << value << endl;
+    
+    int userID = 0x12;
+    userID += 5;
+    Cout << userID << Endl; // C=c E=e
+     
+     
+     return 0;
+}
